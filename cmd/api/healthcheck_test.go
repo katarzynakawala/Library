01cca52c8855ba745d9d0a1d7bc7875d5eeec150
Katarzyna Kawala <kat.kawala@gmail.com>
2022-09-16 19:10:36 +0200

    adding healthcheckHandler function plus happy path test

---
+++ cmd/api/healthcheck_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_application_healthcheckHandler(t *testing.T) {
+	var cfg config
+	cfg.port = 8080
+	cfg.env = "development"
+
+	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/v1/healthcheck", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app.healthcheckHandler(rr, r) 
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	assert.Contains(t, string(body),"status: available")
+	assert.Contains(t, string(body),"environment: development")
+	assert.Contains(t, string(body),"version: 1.0.0")
+}
