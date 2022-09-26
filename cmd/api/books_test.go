f58f7409862bb29f4006034ccca6b0cbd6955d98
Katarzyna Kawala <kat.kawala@gmail.com>
2022-09-26 20:47:11 +0200

    making bad requests error messages more readable

--- cmd/api/books_test.go
+++ cmd/api/books_test.go
@@ -35,7 +35,7 @@ func Test_application_showBookHandler(t *testing.T) {
 	assert.Contains(t, string(body), "the requested resource could not be found")
 }
 
-func Test_application_createBookHandler(t *testing.T) {
+func Test_application_createBookHandlerHappyPath(t *testing.T) {
 	app := applicationInstance(4000, "localhost")
 
 	rr := httptest.NewRecorder()
@@ -60,5 +60,34 @@ func Test_application_createBookHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t, string(body), "{Title:book Author:author Year:2016 Pages:107 Genres:[animation adventure]}\n")
+	assert.Equal(t, "{Title:book Author:author Year:2016 Pages:107 Genres:[animation adventure]}\n", string(body))
+}
+
+
+func Test_application_createBookHandlerBadJsonFormat(t *testing.T) {
+	app := applicationInstance(4000, "localhost")
+
+	rr := httptest.NewRecorder()
+
+	requestBody := "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Alex</to></note>"
+	reader := strings.NewReader(requestBody)
+
+	r, err := http.NewRequest(http.MethodPost, "/v1/books", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	app.createBookHandler(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, http.StatusBadRequest, rs.StatusCode)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "{\n\t\"error\": \"body contains badly-formed JSON (at character 1\"\n}\n", string(body))
 }
