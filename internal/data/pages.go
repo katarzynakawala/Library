8571ae1929783fdacb4758ec705b365c78100be1
Katarzyna Kawala <kat.kawala@gmail.com>
2022-09-27 23:00:44 +0200

    adding custome json decoding for field pages

--- internal/data/pages.go
+++ internal/data/pages.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidPagesFormat = errors.New("invalid format of pages")
 
 type Pages int32
 
@@ -13,4 +17,25 @@ func (p Pages) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (p *Pages) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidPagesFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "pages" {
+		return ErrInvalidPagesFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidPagesFormat
+	}
+
+	*p = Pages(i)
+	return nil
 }
