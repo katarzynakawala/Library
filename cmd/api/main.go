4e9d7466127b2c57f2b55e054e0cc2112921290a
Katarzyna Kawala <kat.kawala@gmail.com>
2022-09-30 23:04:28 +0200

    adding first migration files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dns, "db-dsn", os.Getenv("BOOKS_DB_DSN") + "sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dns, "db-dsn", os.Getenv("LIBRARY_DB_DSN") + "sslmode=disable", "PostgreSQL DSN")
 
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
